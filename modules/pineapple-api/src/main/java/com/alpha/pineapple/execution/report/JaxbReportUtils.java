d5438a12807feba72c22b6e99b0979d6e5da6c50
Allan Thrane Andersen <einheriii@gmail.com>
2017-06-17 17:13:43 +0200

    Failure to support reporting on container maps with null values, closes #156

--- modules/pineapple-api/src/main/java/com/alpha/pineapple/execution/report/JaxbReportUtils.java
+++ modules/pineapple-api/src/main/java/com/alpha/pineapple/execution/report/JaxbReportUtils.java
@@ -47,261 +47,264 @@ import com.alpha.pineapple.execution.ExecutionResult;
  */
 public class JaxbReportUtils {
 
-    /**
-     * Logger object.
-     */
-    static Logger logger = Logger.getLogger(JaxbReportUtils.class.getName());
+	/**
+	 * Logger object.
+	 */
+	static Logger logger = Logger.getLogger(JaxbReportUtils.class.getName());
 
-    /**
-     * Create report from JAXB generated object structure by generating a
-     * structure of {@linkplain ExecutionResult} for all properties and child
-     * objects.
-     * 
-     * @param result
-     *            root execution result from which the child structure as added.
-     * @param jaxbObject
-     *            JAXB generated object from which the report is generated.
-     * @param methodMatcher
-     *            method matcher which identifies getter methods to report on.
-     * @param mapsPackageName
-     *            Java package name where custom map implementations are located
-     *            to support binding to maps instead of lists.
-     * 
-     * @throws Exception
-     *             if reporting fails.
-     */
-    @SuppressWarnings("unchecked")
-    public static void reportOnObject(ExecutionResult result, Object jaxbObject, GetterMethodMatcher methodMatcher,
-	    String mapsPackageName) throws Exception {
+	/**
+	 * Create report from JAXB generated object structure by generating a
+	 * structure of {@linkplain ExecutionResult} for all properties and child
+	 * objects.
+	 * 
+	 * @param result
+	 *            root execution result from which the child structure as added.
+	 * @param jaxbObject
+	 *            JAXB generated object from which the report is generated.
+	 * @param methodMatcher
+	 *            method matcher which identifies getter methods to report on.
+	 * @param mapsPackageName
+	 *            Java package name where custom map implementations are located
+	 *            to support binding to maps instead of lists.
+	 * 
+	 * @throws Exception
+	 *             if reporting fails.
+	 */
+	@SuppressWarnings("unchecked")
+	public static void reportOnObject(ExecutionResult result, Object jaxbObject, GetterMethodMatcher methodMatcher,
+			String mapsPackageName) throws Exception {
 
-	// get getter methods and iterate
-	Method[] getterMethods = resolveGetterMethods(jaxbObject, methodMatcher);
-	for (Method method : getterMethods) {
-	    if (!isMethodWithNoParameters(method))
-		continue;
+		// get getter methods and iterate
+		Method[] getterMethods = resolveGetterMethods(jaxbObject, methodMatcher);
+		for (Method method : getterMethods) {
+			if (!isMethodWithNoParameters(method))
+				continue;
 
-	    // property name and value
-	    String propertyName = methodMatcher.resolveAttributeFromGetterMethod(method);
-	    Object value = invokeMethodWithNoArgs(jaxbObject, method);
+			// property name and value
+			String propertyName = methodMatcher.resolveAttributeFromGetterMethod(method);
+			Object value = invokeMethodWithNoArgs(jaxbObject, method);
 
-	    // process string, primitive and primitive as object
-	    if ((methodReturnsString(method)) || (methodReturnsPrimitive(method))
-		    || (methodReturnsPrimitiveAsObject(method))) {
-		reportPrimitiveAndString(result, propertyName, value);
-		continue;
-	    }
+			// process string, primitive and primitive as object
+			if ((methodReturnsString(method)) || (methodReturnsPrimitive(method))
+					|| (methodReturnsPrimitiveAsObject(method))) {
+				reportPrimitiveAndString(result, propertyName, value);
+				continue;
+			}
 
-	    // handle list object
-	    if (methodReturnsObject(method, List.class)) {
-		ExecutionResult childResult = result.addChild(propertyName);
-		List<String> valueList = List.class.cast(value);
-		reportList(childResult, methodMatcher, mapsPackageName, propertyName, valueList);
-		childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-		continue;
-	    }
+			// handle list object
+			if (methodReturnsObject(method, List.class)) {
+				ExecutionResult childResult = result.addChild(propertyName);
+				List<String> valueList = List.class.cast(value);
+				reportList(childResult, methodMatcher, mapsPackageName, propertyName, valueList);
+				childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+				continue;
+			}
 
-	    // handle map object
-	    if (methodReturnsObjectInPackage(method, mapsPackageName)) {
-		ExecutionResult childResult = result.addChild(propertyName);
-		Map<String, Object> valueMap = Map.class.cast(value);
-		reportMap(childResult, methodMatcher, mapsPackageName, propertyName, valueMap);
-		childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-		continue;
-	    }
+			// handle map object
+			if (methodReturnsObjectInPackage(method, mapsPackageName)) {
+				ExecutionResult childResult = result.addChild(propertyName);
+				Map<String, Object> valueMap = Map.class.cast(value);
+				reportMap(childResult, methodMatcher, mapsPackageName, propertyName, valueMap);
+				childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+				continue;
+			}
 
-	    // process object
-	    if (value != null) {
-		ExecutionResult childResult = result.addChild(propertyName);
-		reportOnObject(childResult, value, methodMatcher, mapsPackageName);
-		childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-		continue;
-	    }
+			// process object
+			if (value != null) {
+				ExecutionResult childResult = result.addChild(propertyName);
+				reportOnObject(childResult, value, methodMatcher, mapsPackageName);
+				childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+				continue;
+			}
 
-	    reportPrimitiveAndString(result, propertyName, value);
+			reportPrimitiveAndString(result, propertyName, value);
 
-	}
-    }
-
-    /**
-     * Report on map.
-     * 
-     * @param result
-     *            result to report to.
-     * @param methodMatcher
-     *            method matcher which identifies getter methods to report on.
-     * @param mapsPackageName
-     *            Java package name where custom map implementations are located
-     *            to support binding to maps instead of lists.
-     * @param propertyName
-     *            property name to report.
-     * @param value
-     *            list to report.
-     * @throws Exception
-     *             if reporting fails
-     */
-    static void reportMap(ExecutionResult result, GetterMethodMatcher methodMatcher, String mapsPackageName,
-	    String propertyName, Map<String, Object> value) throws Exception {
-
-	// handle null case
-	if (value == null) {
-	    String valueAsString = createReportFriendlyString(value);
-	    result.addMessage(propertyName, valueAsString);
-	    return;
+		}
 	}
 
-	// handle defined map
-	for (String key : value.keySet()) {
-	    Object mapValue = value.get(key);
-	    ExecutionResult childResult = result.addChild(key);
-	    reportOnObject(childResult, mapValue, methodMatcher, mapsPackageName);
-	    childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-	}
-    }
+	/**
+	 * Report on map.
+	 * 
+	 * @param result
+	 *            result to report to.
+	 * @param methodMatcher
+	 *            method matcher which identifies getter methods to report on.
+	 * @param mapsPackageName
+	 *            Java package name where custom map implementations are located
+	 *            to support binding to maps instead of lists.
+	 * @param propertyName
+	 *            property name to report.
+	 * @param value
+	 *            list to report.
+	 * @throws Exception
+	 *             if reporting fails
+	 */
+	static void reportMap(ExecutionResult result, GetterMethodMatcher methodMatcher, String mapsPackageName,
+			String propertyName, Map<String, Object> value) throws Exception {
 
-    /**
-     * Report on list.
-     * 
-     * @param result
-     *            result to report to.
-     * @param methodMatcher
-     *            method matcher which identifies getter methods to report on.
-     * @param mapsPackageName
-     *            Java package name where custom map implementations are located
-     *            to support binding to maps instead of lists.
-     * @param propertyName
-     *            property name to report.
-     * @param value
-     *            list to report.
-     * @throws Exception
-     *             if reporting fails
-     */
-    static void reportList(ExecutionResult result, GetterMethodMatcher methodMatcher, String mapsPackageName,
-	    String propertyName, List<String> value) throws Exception {
-
-	// handle null case
-	if (value == null) {
-	    String valueAsString = createReportFriendlyString(value);
-	    result.addMessage(propertyName, valueAsString);
-	    return;
-	}
-
-	// handle defined list
-	for (Object listValue : value) {
-	    String valueAsString = createReportFriendlyString(listValue);
-	    ExecutionResult childResult = result.addChild(valueAsString);
-	    reportOnObject(childResult, listValue, methodMatcher, mapsPackageName);
-	    childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-	}
-    }
-
-    /**
-     * Report on primitive and string.
-     * 
-     * @param result
-     *            result to report to.
-     * @param propertyName
-     *            property name to report
-     * @param value
-     *            property value to report.
-     */
-    static void reportPrimitiveAndString(ExecutionResult result, String propertyName, Object value) {
-	String valueAsString = createReportFriendlyString(value);
-	result.addMessage(propertyName, valueAsString);
-    }
-
-    /**
-     * Test JAXB generated object structure by generating a structure of
-     * {@linkplain ExecutionResult} for all properties and child objects.
-     * 
-     * @param result
-     *            root execution result from which the child structure as added.
-     * @param jaxbObjExpected
-     *            JAXB generated object from which contains the expected values.
-     * @param jaxbObjActual
-     *            JAXB generated object from which contains the actual values.
-     * @param methodMatcher
-     *            method matcher which identifies getter methods to report on.
-     * @param mapsPackageName
-     *            Java package name where custom map implementations are located
-     *            to support binding to maps instead of lists.
-     * 
-     * @throws Exception
-     *             if reporting fails.
-     */
-    @SuppressWarnings("unchecked")
-    public static void testOnObjects(ExecutionResult result, Object jaxbObjExpected, Object jaxbObjActual,
-	    GetterMethodMatcher methodMatcher, String mapsPackageName) throws Exception {
-
-	// get getter methods from expected
-	Method[] getterMethodsExpected = resolveGetterMethods(jaxbObjExpected, methodMatcher);
-
-	// get getter methods from actual
-	Method[] getterMethodsActual = resolveGetterMethods(jaxbObjActual, methodMatcher);
-	Map<String, Method> getterMethodsActualMap = new HashMap<>();
-	for (Method m : getterMethodsActual) {
-	    getterMethodsActualMap.put(m.getName(), m);
-	}
-
-	for (Method method : getterMethodsExpected) {
-	    if (!isMethodWithNoParameters(method))
-		continue;
-
-	    // resolved expected property name and value
-	    String propertyName = methodMatcher.resolveAttributeFromGetterMethod(method);
-	    Object expectedValue = invokeMethodWithNoArgs(jaxbObjExpected, method);
-
-	    // skip if no actual value is defined.
-	    if (!getterMethodsActualMap.containsKey(method.getName())) {
-		continue;
-	    }
-	    // resolve actual value
-	    Method actualMethod = getterMethodsActualMap.get(method.getName());
-	    Object actualValue = invokeMethodWithNoArgs(jaxbObjActual, actualMethod);
-
-	    // process string, primitive and primitive as object
-	    if ((methodReturnsString(method)) || (methodReturnsPrimitive(method))
-		    || (methodReturnsPrimitiveAsObject(method))) {
-
-		if (expectedValue == null) {
-		    reportPrimitiveAndString(result, propertyName, expectedValue);
-		    continue;
+		// handle null case
+		if (value == null) {
+			String valueAsString = createReportFriendlyString(value);
+			result.addMessage(propertyName, valueAsString);
+			return;
 		}
 
-	    }
-
-	    // handle list object
-	    if (methodReturnsObject(method, List.class)) {
-		/**
-		 * List<String> valueAsList = (List<String>) expectedValue;
-		 * String listAsString = Arrays.toString(valueAsList.toArray());
-		 * String valueAsString =
-		 * createReportFriendlyString(listAsString);
-		 * result.addMessage(propertyName,valueAsString);
-		 **/
-		continue;
-	    }
-
-	    // handle map object
-	    if (methodReturnsObjectInPackage(method, mapsPackageName)) {
-		/**
-		 * String valueAsString =
-		 * createReportFriendlyString(expectedValue);
-		 * result.addMessage(propertyName,valueAsString);
-		 **/
-		continue;
-	    }
-
-	    // process object
-	    if (expectedValue != null) {
-		ExecutionResult childResult = result.addChild(propertyName);
-		testOnObjects(childResult, expectedValue, actualValue, methodMatcher, mapsPackageName);
-		childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
-		continue;
-	    }
-
-	    reportPrimitiveAndString(result, propertyName, expectedValue);
-
+		// handle defined map
+		for (String key : value.keySet()) {
+			Object mapValue = value.get(key);
+			ExecutionResult childResult = result.addChild(key);
+			
+			// null value
+			if( mapValue != null) reportOnObject(childResult, mapValue, methodMatcher, mapsPackageName);
+			else childResult.addMessage(key, "null");
+			childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+		}
 	}
-    }
+
+	/**
+	 * Report on list.
+	 * 
+	 * @param result
+	 *            result to report to.
+	 * @param methodMatcher
+	 *            method matcher which identifies getter methods to report on.
+	 * @param mapsPackageName
+	 *            Java package name where custom map implementations are located
+	 *            to support binding to maps instead of lists.
+	 * @param propertyName
+	 *            property name to report.
+	 * @param value
+	 *            list to report.
+	 * @throws Exception
+	 *             if reporting fails
+	 */
+	static void reportList(ExecutionResult result, GetterMethodMatcher methodMatcher, String mapsPackageName,
+			String propertyName, List<String> value) throws Exception {
+
+		// handle null case
+		if (value == null) {
+			String valueAsString = createReportFriendlyString(value);
+			result.addMessage(propertyName, valueAsString);
+			return;
+		}
+
+		// handle defined list
+		for (Object listValue : value) {
+			String valueAsString = createReportFriendlyString(listValue);
+			ExecutionResult childResult = result.addChild(valueAsString);
+			reportOnObject(childResult, listValue, methodMatcher, mapsPackageName);
+			childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+		}
+	}
+
+	/**
+	 * Report on primitive and string.
+	 * 
+	 * @param result
+	 *            result to report to.
+	 * @param propertyName
+	 *            property name to report
+	 * @param value
+	 *            property value to report.
+	 */
+	static void reportPrimitiveAndString(ExecutionResult result, String propertyName, Object value) {
+		String valueAsString = createReportFriendlyString(value);
+		result.addMessage(propertyName, valueAsString);
+	}
+
+	/**
+	 * Test JAXB generated object structure by generating a structure of
+	 * {@linkplain ExecutionResult} for all properties and child objects.
+	 * 
+	 * @param result
+	 *            root execution result from which the child structure as added.
+	 * @param jaxbObjExpected
+	 *            JAXB generated object from which contains the expected values.
+	 * @param jaxbObjActual
+	 *            JAXB generated object from which contains the actual values.
+	 * @param methodMatcher
+	 *            method matcher which identifies getter methods to report on.
+	 * @param mapsPackageName
+	 *            Java package name where custom map implementations are located
+	 *            to support binding to maps instead of lists.
+	 * 
+	 * @throws Exception
+	 *             if reporting fails.
+	 */
+	@SuppressWarnings("unchecked")
+	public static void testOnObjects(ExecutionResult result, Object jaxbObjExpected, Object jaxbObjActual,
+			GetterMethodMatcher methodMatcher, String mapsPackageName) throws Exception {
+
+		// get getter methods from expected
+		Method[] getterMethodsExpected = resolveGetterMethods(jaxbObjExpected, methodMatcher);
+
+		// get getter methods from actual
+		Method[] getterMethodsActual = resolveGetterMethods(jaxbObjActual, methodMatcher);
+		Map<String, Method> getterMethodsActualMap = new HashMap<>();
+		for (Method m : getterMethodsActual) {
+			getterMethodsActualMap.put(m.getName(), m);
+		}
+
+		for (Method method : getterMethodsExpected) {
+			if (!isMethodWithNoParameters(method))
+				continue;
+
+			// resolved expected property name and value
+			String propertyName = methodMatcher.resolveAttributeFromGetterMethod(method);
+			Object expectedValue = invokeMethodWithNoArgs(jaxbObjExpected, method);
+
+			// skip if no actual value is defined.
+			if (!getterMethodsActualMap.containsKey(method.getName())) {
+				continue;
+			}
+			// resolve actual value
+			Method actualMethod = getterMethodsActualMap.get(method.getName());
+			Object actualValue = invokeMethodWithNoArgs(jaxbObjActual, actualMethod);
+
+			// process string, primitive and primitive as object
+			if ((methodReturnsString(method)) || (methodReturnsPrimitive(method))
+					|| (methodReturnsPrimitiveAsObject(method))) {
+
+				if (expectedValue == null) {
+					reportPrimitiveAndString(result, propertyName, expectedValue);
+					continue;
+				}
+
+			}
+
+			// handle list object
+			if (methodReturnsObject(method, List.class)) {
+				/**
+				 * List<String> valueAsList = (List<String>) expectedValue;
+				 * String listAsString = Arrays.toString(valueAsList.toArray());
+				 * String valueAsString =
+				 * createReportFriendlyString(listAsString);
+				 * result.addMessage(propertyName,valueAsString);
+				 **/
+				continue;
+			}
+
+			// handle map object
+			if (methodReturnsObjectInPackage(method, mapsPackageName)) {
+				/**
+				 * String valueAsString =
+				 * createReportFriendlyString(expectedValue);
+				 * result.addMessage(propertyName,valueAsString);
+				 **/
+				continue;
+			}
+
+			// process object
+			if (expectedValue != null) {
+				ExecutionResult childResult = result.addChild(propertyName);
+				testOnObjects(childResult, expectedValue, actualValue, methodMatcher, mapsPackageName);
+				childResult.setState(ExecutionResult.ExecutionState.SUCCESS);
+				continue;
+			}
+
+			reportPrimitiveAndString(result, propertyName, expectedValue);
+
+		}
+	}
 
 }
